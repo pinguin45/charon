28b510cc801b8d4d5a29eb2cb5daa85b159fafd2
Alexander Kasten <alexander.kasten@5minds.de>
2017-08-10 12:54:43 +0200

    :sparkles: Add processengine interface

---
+++ src/contracts/processengine.ts
@@ -0,0 +1,19 @@
+export interface Pagination<T> {
+    count: number;
+    offset: number;
+    limit: number;
+    data: Array<T>;
+}
+
+export interface ProcessDef {
+    id: string;
+    name: string;
+    key: string;
+    defID: string;
+    xml: string;
+    readonly?: boolean;
+    version: string;
+}
+
+export interface ProcessDefPagination extends Pagination<ProcessDef> {
+}
