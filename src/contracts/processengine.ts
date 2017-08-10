335be07a5e8dc544a79883b7cdcee14a0aeef78c
Alexander Kasten <alexander.kasten@5minds.de>
2017-08-10 12:58:28 +0200

    :lipstick: Change spaces

--- src/contracts/processengine.ts
+++ src/contracts/processengine.ts
@@ -1,18 +1,18 @@
 export interface Pagination<T> {
-    count: number;
-    offset: number;
-    limit: number;
-    data: Array<T>;
+  count: number;
+  offset: number;
+  limit: number;
+  data: Array<T>;
 }
 
 export interface ProcessDef {
-    id: string;
-    name: string;
-    key: string;
-    defID: string;
-    xml: string;
-    readonly?: boolean;
-    version: string;
+  id: string;
+  name: string;
+  key: string;
+  defID: string;
+  xml: string;
+  readonly?: boolean;
+  version: string;
 }
 
 export interface ProcessDefPagination extends Pagination<ProcessDef> {
