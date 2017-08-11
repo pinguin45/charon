b50643074ceea52e5312777d819a540a12f7c77c
Alexander Kasten <alexander.kasten@5minds.de>
2017-08-11 10:50:04 +0200

    :sparkles: Add first UI for processlist

--- src/modules/processlist/processlist.ts
+++ src/modules/processlist/processlist.ts
@@ -1,12 +1,23 @@
-import {autoinject, inject} from 'aurelia-framework';
+import {inject, computedFrom} from 'aurelia-framework';
 import {ProcessDef, IProcessEngineService} from '../../contracts';
 
 @inject('ProcessEngineService')
 export class Processlist {
 
   private processEngineService: IProcessEngineService;
+  private _processes: Array<ProcessDef> = [];
 
-  public constructor(processEngineService: IProcessEngineService) {
+  constructor(processEngineService: IProcessEngineService) {
     this.processEngineService = processEngineService;
+    this.processEngineService.getProcesses()
+      .then((result: Array<ProcessDef>) => {
+        this._processes = result;
+      });
   }
+
+  @computedFrom('_processes')
+  public get processes(): Array<ProcessDef> {
+    return this._processes;
+  }
+
 }
