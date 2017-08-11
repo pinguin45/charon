06a147f940b58a97a69feb641ca100195e2250dc
Paul <paul.heidenreich@5minds.de>
2017-08-11 15:05:03 +0200

    :lipstick: Fix formating issues

--- src/modules/processengine/index.ts
+++ src/modules/processengine/index.ts
@@ -2,7 +2,7 @@ import {FrameworkConfiguration} from 'aurelia-framework';
 import {ProcessEngineRepository} from './processengine.repository';
 import {ProcessEngineService} from './processengine.service';
 
-export function configure(config: FrameworkConfiguration) {
-    config.container.registerSingleton('ProcessEngineRepository', ProcessEngineRepository);
-    config.container.registerSingleton('ProcessEngineService', ProcessEngineService);
+export function configure(config: FrameworkConfiguration): void {
+  config.container.registerSingleton('ProcessEngineRepository', ProcessEngineRepository);
+  config.container.registerSingleton('ProcessEngineService', ProcessEngineService);
 }
