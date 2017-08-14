a7b4a479402470077807a46d99a96d9efedab5b7
Alexander Kasten <alexander.kasten@5minds.de>
2017-08-14 15:33:38 +0200

    :sparkles: Add BPMN.io modeler

---
+++ src/contracts/bpmnmodeler.ts
@@ -0,0 +1,67 @@
+export interface IBpmnModelerConstructor {
+  new(options: {
+    additionalModules?: Array<IDependencyHook>,
+    container?: string,
+  }): IBpmnModeler;
+}
+
+export interface IDependencyHook {
+  __depends__: Array<string>;
+  __init__: Array<string>;
+  [index: string]: [string, any] | Array<string>;
+}
+
+export interface IBpmnModeler {
+  attachTo(wrapper: HTMLElement): void;
+  importXML(xml: string,
+            errorHandler: (err: Error) => void): void;
+}
+
+export interface IEventBus {
+  on(events: Array<string> | string,
+     priority: number,
+     callback: Function,
+     callbackScope?: any): void;
+  on(events: Array<string> | string,
+     callback: Function,
+     callbackScope?: any): void;
+
+  once(events: Array<string> | string,
+       priority: number,
+       callback: Function,
+       callbackScope: any): void;
+  once(events: Array<string> | string,
+       callback: Function,
+       callbackScope: any): void;
+
+  off(event: string,
+      callback?: Function): void;
+
+  fire(name: string,
+       data?: any): any;
+  fire(eventObject: { type: string }, // tslint:disable-line
+       data?: any): any;
+}
+
+export interface IModdleElement {
+  id: string;
+  get: any;
+  $type: string;
+  $attrs?: any;
+  $parent?: IModdleElement;
+}
+
+export interface IShape {
+  businessObject: IModdleElement;
+  id: string;
+  type: string;
+  label: IShape;
+  x: number;
+  y: number;
+  width: number;
+  height: number;
+}
+
+export interface ICanvas {
+  getRootElement(): IShape;
+}
