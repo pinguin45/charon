3206edef05c0d9f8a0cca923d515a1c6e593bc33
Paul <paul.heidenreich@5minds.de>
2017-08-15 15:11:40 +0200

    :construction: WIP on bpmnio

--- src/modules/bpmn-io/bpmn-io.ts
+++ src/modules/bpmn-io/bpmn-io.ts
@@ -1,11 +1,13 @@
 import {IBpmnModeler, IBpmnModelerConstructor} from '../../contracts';
+
+import * as x from '@process-engine-js/bpmn-js-custom-bundle';
 import {startdiagram} from './bpmn-xml';
 
 export class BpmnIo {
 
   private attached(): void {
     // bundle exposes the viewer / modeler via the BpmnJS variable
-    const BpmnModeler: IBpmnModelerConstructor  = (window as any).BpmnJS;
+    const BpmnModeler: IBpmnModelerConstructor = x;
     const modeler: IBpmnModeler = new BpmnModeler({
       container: '#canvas',
       propertiesPanel: {
