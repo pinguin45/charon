27f919832b435142b634de5ac961ea3168472115
Paul <paul.heidenreich@5minds.de>
2017-08-15 11:11:16 +0200

    :makeup: Cleanup code style

--- src/modules/bpmn-io/bpmn-io.ts
+++ src/modules/bpmn-io/bpmn-io.ts
@@ -1,34 +1,19 @@
-// import {computedFrom, inject } from 'aurelia-framework';
-// import * as Modeler from 'bpmn-js';
-
-// //@inject('ProcessEngineService')
-// export class BPMNIO {
-
-//     private modeler: Modeler;
-
-//     constructor(modeler: Modeler) {
-//         this.modeler = new Modeler({ container: '#canvas'});
-//     }
-
-//     // public importDiagram(): void {
-//     //     Modeler.importXML(bpmnXML, {
-
-//     //     });
-//     // }
-
-// }
-import { IBpmnModeler, IBpmnModelerConstructor } from '../../contracts';
-import { startdiagram } from './bpmn-xml';
+import {IBpmnModeler, IBpmnModelerConstructor} from '../../contracts';
+import {startdiagram} from './bpmn-xml';
 
 export class BpmnIo {
 
-    private attached(): void {
-        // bundle exposes the viewer / modeler via the BpmnJS variable
-        const BpmnModeler: IBpmnModelerConstructor  = (window as any).BpmnJS;
-        const modeler: IBpmnModeler = new BpmnModeler({ container: '#canvas' });
-        modeler.importXML(startdiagram, (err: Error) => {
-        return 0;
-        });
-    }
-
+  private attached(): void {
+    // bundle exposes the viewer / modeler via the BpmnJS variable
+    const BpmnModeler: IBpmnModelerConstructor  = (window as any).BpmnJS;
+    const modeler: IBpmnModeler = new BpmnModeler({
+      container: '#canvas',
+      propertiesPanel: {
+        parent: '#properties'
+      },
+    });
+    modeler.importXML(startdiagram, (err: Error) => {
+      return 0;
+    });
+  }
 }
