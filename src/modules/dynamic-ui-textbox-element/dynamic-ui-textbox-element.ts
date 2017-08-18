38664d49fcea22484c572c8a69aa1a49eb0f5a11
Alexander Kasten <alexander.kasten@5minds.de>
2017-08-18 17:05:07 +0200

    :sparkles: Add attribute bindings for textbox element

--- src/modules/dynamic-ui-textbox-element/dynamic-ui-textbox-element.ts
+++ src/modules/dynamic-ui-textbox-element/dynamic-ui-textbox-element.ts
@@ -1,3 +1,7 @@
-export class DynamicUiTextboxElement {
+import {bindable} from 'aurelia-framework';
 
+export class DynamicUiTextboxElement {
+  @bindable() private label: string;
+  @bindable() private defaultValue: string;
+  @bindable() private id: string;
 }
