b6c4d88b714d1b1dfecc446111f1a81ac2039b35
Alexander Kasten <alexander.kasten@5minds.de>
2017-08-21 10:01:13 +0200

    :sparkles: Change for using compose

--- src/modules/dynamic-ui-checkbox-element/dynamic-ui-checkbox-element.ts
+++ src/modules/dynamic-ui-checkbox-element/dynamic-ui-checkbox-element.ts
@@ -1,3 +1,11 @@
 export class DynamicUiCheckboxElement {
+  private label: string;
+  private defaultValue: string;
+  private id: string;
 
+  private activate(model: any): void {
+    this.label = model.label;
+    this.defaultValue = model.defaultValue;
+    this.id = model.id;
+  }
 }
--- src/modules/dynamic-ui-textbox-element/dynamic-ui-textbox-element.ts
+++ src/modules/dynamic-ui-textbox-element/dynamic-ui-textbox-element.ts
@@ -1,7 +1,11 @@
-import {bindable} from 'aurelia-framework';
-
 export class DynamicUiTextboxElement {
-  @bindable() private label: string;
-  @bindable() private defaultValue: string;
-  @bindable() private id: string;
+  private label: string;
+  private defaultValue: string;
+  private id: string;
+
+  private activate(model: any): void {
+    this.label = model.label;
+    this.defaultValue = model.defaultValue;
+    this.id = model.id;
+  }
 }
