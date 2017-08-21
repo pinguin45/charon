d289f69b6e7a8978f948e1772fc48123fa57f9af
Alexander Kasten <alexander.kasten@5minds.de>
2017-08-21 10:29:16 +0200

    :sparkles: Add placeholder to textbox element

--- src/modules/dynamic-ui-textbox-element/dynamic-ui-textbox-element.ts
+++ src/modules/dynamic-ui-textbox-element/dynamic-ui-textbox-element.ts
@@ -2,10 +2,12 @@ export class DynamicUiTextboxElement {
   private label: string;
   private defaultValue: string;
   private id: string;
+  private placeholder: string;
 
   private activate(model: any): void {
     this.label = model.label;
     this.defaultValue = model.defaultValue;
     this.id = model.id;
+    this.placeholder = model.placeholder;
   }
 }
