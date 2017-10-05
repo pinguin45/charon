7aa59cef1e5c83821e6c9dabde5f86da07db4509
Paul <paul.heidenreich@5minds.de>
2017-10-05 15:43:32 +0200

    :sparkles: Only handle incomming message for roles that the user has

--- src/modules/messagebus/messagebus.service.ts
+++ src/modules/messagebus/messagebus.service.ts
@@ -18,6 +18,11 @@ export class MessageBusService implements IMessageBusService {
   }
 
   private handleIncommingMessage(channel: string, message: any): void {
+    const isAllowedToHandle: boolean = this.isAllowedToHandle(channel);
+    if (!isAllowedToHandle) {
+      return;
+    }
+
     for (const handler of this.messageHandlers) {
       handler(channel, message);
     }
@@ -31,6 +36,24 @@ export class MessageBusService implements IMessageBusService {
       };
     }
     return message;
+  }
+
+  private isAllowedToHandle(channel: string): boolean {
+    let roles: Array<string> = ['guest']; // default roles
+    if (this.authenticationService.getIdentity()) {
+      roles = this.authenticationService.getIdentity().roles;
+    }
+
+    const rolePrefix: string = '/role/';
+    if (channel.startsWith(rolePrefix)) {
+      const handledRole: string = channel.substr(rolePrefix.length);
+      for (const role of roles) {
+        if (handledRole === role) {
+          return true;
+        }
+      }
+    }
+    return false;
   }
 
   public sendMessage(channel: string, message: any): Promise<any> {
