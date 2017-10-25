783e5e007674ce947f1324434ecd78d0a84a86b4
bastian schnittger <bastian.schnittger@5minds.de>
2017-10-25 16:18:16 +0200

    remove config

--- src/modules/consumer-client/index.ts
+++ src/modules/consumer-client/index.ts
@@ -4,7 +4,6 @@ import {FrameworkConfiguration} from 'aurelia-framework';
 export async function configure(config: FrameworkConfiguration, tokenRepository: ITokenRepository): Promise<void> {
 
   const consumerClient: ConsumerClient = new ConsumerClient();
-  consumerClient.config = config;
   await consumerClient.initialize(tokenRepository);
 
   config.container.registerInstance('ConsumerClient', consumerClient);
