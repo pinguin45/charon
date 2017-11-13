9aa2bb3cee633d4c66f9d98d9098469815e15104
Alexander Kasten <alexander.kasten@5minds.de>
2017-11-13 10:00:01 +0100

    :fire: Remove unnecessary stuff from mustache

--- src/modules/confirm-widget/confirm-widget.ts
+++ src/modules/confirm-widget/confirm-widget.ts
@@ -4,8 +4,6 @@ export class ConfirmWidget {
 
   @bindable()
   private widget: any;
-  private template: HTMLElement;
-  private target: HTMLLabelElement;
 
   private activate(widget: any): void {
     this.widget = widget;
