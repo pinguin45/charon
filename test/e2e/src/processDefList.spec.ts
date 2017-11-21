38c08e748ce95f3212f92b5ea284430b1913edc0
bastian schnittger <bastian.schnittger@5minds.de>
2017-11-21 16:27:42 +0100

    fix minor typo

--- test/e2e/src/processDefList.spec.ts
+++ test/e2e/src/processDefList.spec.ts
@@ -4,16 +4,15 @@ import {ProcessDefListPage} from './pages/processdef-list-page';
 
 describe('processDefList', () => {
 
-  const defaultTimeout: number = 5000;
+  const defaultTimeout: number = 1000;
   const aureliaUrl: string = 'http://localhost:9000';
 
   browser.driver.manage().deleteAllCookies();
   browser.get(aureliaUrl);
 
-
   it('should display process definitions', () => {
     const processDefListPage: ProcessDefListPage = new ProcessDefListPage();
-    browser.sleep(1000);
+    browser.sleep(defaultTimeout);
     expect(processDefListPage.processDefs.count()).toBeGreaterThan(0);
   });
 
